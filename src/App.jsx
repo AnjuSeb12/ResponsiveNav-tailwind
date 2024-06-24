0881971821ad1d443875b7070222531814465ef8
Anju Sebastian <christeenaanju@gmail.com>
2024-06-24 13:16:37 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import Nav from './Components/Nav'
+
+
+const App = () => {
+  return (
+    <div className='bg-slate-900'>
+      <Nav/>
+    </div>
+  )
+}
+
+export default App
