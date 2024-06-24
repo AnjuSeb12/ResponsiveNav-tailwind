d6cc1d4092a6da65a230b568d7fce5d0960f775f
Anju Sebastian <christeenaanju@gmail.com>
2024-06-24 13:40:27 +0530

    updated

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -13,19 +13,19 @@ const Nav = () => {
     <div className='lg:hidden block absolute top-16 w-full left-0 right-0 bg-slate-900 transition'>
       <ul className='text-center text-xl p-20'>
         <Link spy={true} smooth={true} to="Home">
-          <li className='my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded'>Home</li>
+          <li className='my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded hover:text-orange-600 cursor-pointer'>Home</li>
         </Link>
         <Link  spy={true} smooth={true} to="About">
-          <li className='my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded'>About</li>
+          <li className='my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded hover:text-orange-600 cursor-pointer'>About</li>
         </Link>
         <Link spy={true} smooth={true} to="Services">
-          <li className='my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded'>Services</li>
+          <li className='my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded hover:text-orange-600 cursor-pointer'>Services</li>
         </Link>
         <Link spy={true} smooth={true} to="Projects">
-          <li className='my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded'>Projects</li>
+          <li className='my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded hover:text-orange-600 cursor-pointer'>Projects</li>
         </Link>
         <Link spy={true} smooth={true}  to="Contact">
-          <li className='my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded'>Contact</li>
+          <li className='my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded hover:text-orange-600 cursor-pointer'>Contact</li>
         </Link>
 
       </ul>
@@ -37,7 +37,7 @@ const Nav = () => {
     <nav>
       <div className='h-10vh flex justify-between z-50 text-white lg:py-5 px-20 py-4'>
         {/* ..... */}
-        <div className='flex items-center flex-1'>
+        <div className='flex items-center flex-1 '>
           <span className='text-3xl font-bold'>Logo</span>
 
         </div>
@@ -48,19 +48,19 @@ const Nav = () => {
           <div className='flex-10'>
             <ul className='flex gap-8 mr-16 text-[18px]'>
               <Link spy={true} smooth={true} to="Home">
-                <li className='hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer'>Home</li>
+                <li className='hover:text-orange-600 transition border-b-2 border-slate-900 hover:border-orange-800 cursor-pointer'>Home</li>
               </Link>
               <Link  spy={true} smooth={true} to="About">
-                <li className='hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer'>About</li>
+                <li className='hover:text-orange-600 transition border-b-2 border-slate-900 hover:border-orange-800 cursor-pointer'>About</li>
               </Link>
               <Link spy={true} smooth={true}  to="Services">
-                <li className='hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer'>Services</li>
+                <li className='hover:text-orange-600 transition border-b-2 border-slate-900 hover:border-orange-800 cursor-pointer'>Services</li>
               </Link>
               <Link spy={true} smooth={true}  to="Projects">
-                <li className='hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer'>Projects</li>
+                <li className='hover:text-orange-600 transition border-b-2 border-slate-900 hover:border-orange-800 cursor-pointer'>Projects</li>
               </Link>
               <Link spy={true} smooth={true} to="Contact">
-                <li className='hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer'>Contact</li>
+                <li className='hover:text-orange-600 transition border-b-2 border-slate-900 hover:border-orange-800 cursor-pointer'>Contact</li>
               </Link>
 
             </ul>
